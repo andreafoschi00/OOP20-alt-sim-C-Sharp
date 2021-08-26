d82ae87f680e067abf5bba5ffd8a5c388cbfcba4
Andrea <andrea.severi12@studio.unibo.it>
2021-08-26 15:19:32 +0200

    create cross platform usability

---
+++ Severi/src/common/OsDependent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace common
+{
+    /// <summary>
+    /// Gets system dependent se
+    /// </summary>
+    public static class OsDependent
+    {
+        /// Gets OS file separator.
+        public static readonly char Separator = Path.DirectorySeparatorChar;
+        
+        /// <summary>
+        /// Gets user home directory.
+        /// </summary>
+        public static readonly string UserHomeDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+    }
+}

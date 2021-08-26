033ca8a45dbd1135ecf6639588ca759c7fb10b1d
Andrea <andrea.severi12@studio.unibo.it>
2021-08-26 16:21:46 +0200

    add score folder

---
+++ Severi/Severi/src/user/score/Score.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Severi.user.score
+{
+    public static class Score
+    {
+        public static int GenerateRandomScore()
+        {
+            var random = new Random();
+            var score = random.Next(0, 3001);
+            var steps = Math.Floor(score / 100.0);
+            var adjustedScore = steps * 100.0;
+
+            return (int) adjustedScore;
+        }
+    }
+}

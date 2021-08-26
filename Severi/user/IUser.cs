6533f88e7e5b750bbf1c176894a108cdfc79d85b
Andrea <andrea.severi12@studio.unibo.it>
2021-08-26 15:16:27 +0200

    create user interface and impl

---
+++ Severi/user/IUser.cs
@@ -0,0 +1,21 @@
+﻿namespace user
+{
+    public interface IUser
+    {
+        /// <summary>
+        /// Gets user name.
+        /// </summary>
+        /// <returns>user name</returns>
+        string Name { get; }
+
+        /// <summary>
+        /// Gets/sets user score.
+        /// </summary>
+        int Score { get; set; }
+
+        /// <summary>
+        /// Resets user score.
+        /// </summary>
+        void ResetScore();
+    }
+}
---
+++ Severi/user/User.cs
@@ -0,0 +1,26 @@
+﻿namespace user
+{
+    public class User : IUser
+    {
+
+        public User(in string name, in int score)
+        {
+            Name = name;
+            Score = score;
+        }
+        
+        /// <inheritdoc />
+        public string Name { get; }
+        
+        /// <inheritdoc />
+        public int Score { get; set; }
+        
+        /// <inheritdoc />
+        public void ResetScore()
+        {
+            Score = 0;
+        }
+
+        public override string ToString() => "User[name: " + Name + ", score: " + Score + "]";
+    }
+}

247ce64c96ca4092fccd7a2665a0c857494c21f3
Andrea <andrea.severi12@studio.unibo.it>
2021-08-26 15:16:12 +0200

    create builder pattern for user

---
+++ Severi/user/builder/IUserBuilder.cs
@@ -0,0 +1,25 @@
+﻿namespace user.builder
+{
+    public interface IUserBuilder
+    {
+        /// <summary>
+        /// Adds name to user at building.
+        /// </summary>
+        /// <param name="name"> of the user</param>
+        /// <returns>builder</returns>
+        IUserBuilder Name(in string name);
+        
+        /// <summary>
+        /// Adds score to user at building.
+        /// </summary>
+        /// <param name="score">of the user</param>
+        /// <returns>builder</returns>
+        IUserBuilder Score(in int score);
+
+        /// <summary>
+        /// Builds user by name and score.
+        /// </summary>
+        /// <returns>user built</returns>
+        IUser Build();
+    }
+}
---
+++ Severi/user/builder/UserBuilder.cs
@@ -0,0 +1,36 @@
+﻿namespace user.builder
+{
+    public class UserBuilder : IUserBuilder
+    {
+        private string _name;
+        private int _score;
+
+        /// <inheritdoc />
+        public IUserBuilder Name(in string name)
+        {
+            _name = name;
+            return this;
+        }
+
+        /// <inheritdoc />
+        public IUserBuilder Score(in int score)
+        {
+            _score = score;
+            return this;
+        }
+
+        /// <inheritdoc />
+        public IUser Build() => _name != null ? new User(_name, _score) : null;
+
+        /// <summary>
+        /// Builds user by name given at the start.
+        /// </summary>
+        /// <param name="name">to add to user</param>
+        /// <param name="score">to add to user</param>
+        /// <returns>built User</returns>
+        public static IUser BuildUser(in string name, in int score)
+        {
+            return new UserBuilder().Name(name).Score(score).Build();
+        }
+    }
+}

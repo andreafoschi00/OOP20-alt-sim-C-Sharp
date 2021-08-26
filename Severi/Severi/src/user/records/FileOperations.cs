58421d471f427ea14d1341abce2d019e77fd2b3e
Andrea <andrea.severi12@studio.unibo.it>
2021-08-26 16:21:32 +0200

    add records folder

---
+++ Severi/Severi/src/user/records/FileOperations.cs
@@ -0,0 +1,85 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using Severi.user.validation;
+
+namespace Severi.user.records
+{
+    public static class FileOperations
+    {
+        
+        public static Dictionary<string, int> Users { get; private set; } = new();
+        
+        /// <summary>
+        /// Creates directory by given path.
+        /// </summary>
+        /// <param name="path">to directory</param>
+        public static void CreateDirectory(in string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+
+        /// <summary>
+        /// Creates file by path.
+        /// </summary>
+        /// <param name="path">to file</param>
+        public static void CreateFile(in string path)
+        {
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+        }
+
+        /// <summary>
+        /// Loads file by path.
+        /// </summary>
+        /// <param name="file">to load</param>
+        private static void LoadFile(in string file)
+        {
+            new RecordsValidation().UserRecordsFileValidation();
+            var jsonString = File.ReadAllText(file);
+            Users = JsonSerializer.Deserialize<Dictionary<string, int>>(jsonString) ?? new Dictionary<string, int>();
+        }
+        
+        /// <summary>
+        /// Updates file by path.
+        /// </summary>
+        /// <param name="file">to update</param>
+        private static void UpdateFile(in string file)
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            var jsonString = JsonSerializer.Serialize(Users, options);
+            new RecordsValidation().UserRecordsFileValidation();
+            File.WriteAllText(file, jsonString);
+        }
+
+        /// <summary>
+        /// Add user to file.
+        /// </summary>
+        /// <param name="user">to add</param>
+        public static void AddUser(in IUser user)
+        {
+            LoadFile(RecordsFolder.UserRecordsFilePath);
+            if (!Users.ContainsKey(user.Name.Trim()))
+            {
+                Users.Add(user.Name.Trim(), user.Score);
+            }
+            UpdateFile(RecordsFolder.UserRecordsFilePath);
+        }
+
+        /// <summary>
+        /// Checks if given is already taken.
+        /// </summary>
+        /// <param name="name">to check</param>
+        /// <returns>true if taken</returns>
+        public static bool IsTaken(in string name)
+        {
+            LoadFile(RecordsFolder.UserRecordsFilePath);
+            return Users.ContainsKey(name);
+        }
+    }
+}
---
+++ Severi/Severi/src/user/records/RecordsFolder.cs
@@ -0,0 +1,22 @@
+using Severi.common;
+
+namespace Severi.user.records
+{
+    public static class RecordsFolder
+    {
+        /// <summary>
+        /// Path to hidden directory.
+        /// </summary>
+        public static readonly string RecordsDirectoryPath = OsDependent.UserHomeDirectory + OsDependent.Separator + ".altsim-c#";
+        
+        /// <summary>
+        /// Path to directory containing users file.
+        /// </summary>
+        public static readonly string UserRecordsDirectoryPath = RecordsDirectoryPath + OsDependent.Separator + "user_records";
+        
+        /// <summary>
+        /// Path to users file.
+        /// </summary>
+        public static readonly string UserRecordsFilePath = UserRecordsDirectoryPath + OsDependent.Separator + "users.json";
+    }
+}

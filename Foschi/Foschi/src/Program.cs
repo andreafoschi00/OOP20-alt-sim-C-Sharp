582719e58c3f54b47546fb62e0c0a76bd231d392
Andrea Foschi <andrea.foschi13@studio.unibo.it>
2021-08-30 18:07:53 +0200

    Improve code

--- Foschi/Foschi/src/Program.cs
+++ Foschi/Foschi/src/Program.cs
@@ -5,13 +5,19 @@ namespace Foschi
 {
     public static class Program
     {
+        private const int StripWidth = 150;
+        private const int StripLength = 80;
+        private const int Strip1InitialScore = 300;
+        private const int Strip2InitialScore = 250;
+        private const double StripRadius = 33.3;
+        private const string MyStripUrl = "Andrea/example.png";
         public static void Main()
         {
-            AirStrip strip1 = new BasicAirStrip("Andrea/example.png", 150, 80);
-            AirStrip strip2 = new HelipadAirStrip(33.3);
+            AirStrip strip1 = new BasicAirStrip(MyStripUrl, StripWidth, StripLength);
+            AirStrip strip2 = new HelipadAirStrip(StripRadius);
 
-            strip1.Score = 300;
-            strip2.Score = 250;
+            strip1.Score = Strip1InitialScore;
+            strip2.Score = Strip2InitialScore;
             strip1.Status = AirStripStatus.Busy;
             strip2.Status = AirStripStatus.Disabled;
             Console.WriteLine("First strip url:{0}, second strip url:{1}", strip1.Url, strip2.Url);

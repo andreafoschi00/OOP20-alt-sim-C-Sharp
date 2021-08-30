906854f904a5591bf934c19581bdfb6a38b0451f
Andrea Foschi <andrea.foschi13@studio.unibo.it>
2021-08-30 16:35:31 +0200

    Create enum AirStripStatus

---
+++ Foschi/Foschi/src/airstrip/AirStripStatus.cs
@@ -0,0 +1,18 @@
+﻿namespace Foschi.airstrip
+{
+    public enum AirStripStatus
+    {
+        /// <summary>
+        /// The airstrip is free and ready for landing planes.
+        /// </summary>
+        Free,
+        /// <summary>
+        /// The airstrip is busy because a plane is landing right now.
+        /// </summary>
+        Busy,
+        /// <summary>
+        /// The airstrip is currently unavailable.
+        /// </summary>
+        Disabled
+    }
+}

7dc15df86038cd203246772529acd94bbac26e3f
Andrea Foschi <andrea.foschi13@studio.unibo.it>
2021-08-30 16:35:40 +0200

    Create HelipadAirStrip.cs

---
+++ Foschi/Foschi/src/airstrip/HelipadAirStrip.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Foschi.airstrip
+{
+    public class HelipadAirStrip: AirStrip
+    {
+        private const double Pi = 3.14;
+        private const int Exponent = 2;
+        /// <summary>
+        /// Radius of the helipad.
+        /// </summary>
+        public double Radius { get; set; }
+
+        public HelipadAirStrip(string url, double radius): base(url)
+        {
+            this.Radius = radius;
+        }
+
+        public HelipadAirStrip(double radius) : this(StandardUrl, radius)
+        {
+            
+        }
+
+        /// <inheritdoc />
+        public override double CalculateArea()
+        {
+            return Pi * Math.Pow(Radius, Exponent);
+        }
+        
+        public override string ToString() => "HelipadAirStrip[Radius: " + Radius + ", Score: " + Score + 
+                                             ", Status: " + Status + "]";
+    }
+}

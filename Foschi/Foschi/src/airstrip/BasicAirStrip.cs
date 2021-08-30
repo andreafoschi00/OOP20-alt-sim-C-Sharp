9dd4e1be6161b074ed02e3e19f59384f5febe201
Andrea Foschi <andrea.foschi13@studio.unibo.it>
2021-08-30 16:35:38 +0200

    Create BasicAirStrip.cs

---
+++ Foschi/Foschi/src/airstrip/BasicAirStrip.cs
@@ -0,0 +1,34 @@
+﻿namespace Foschi.airstrip
+{
+    public class BasicAirStrip: AirStrip
+    {
+        /// <summary>
+        /// Width of the strip.
+        /// </summary>
+        public double Width { get; set; }
+        /// <summary>
+        /// Length of the strip.
+        /// </summary>
+        public double Length { get; set; }
+
+        public BasicAirStrip(string url, double width, double length): base(url)
+        {
+            this.Width = width;
+            this.Length = length;
+        }
+        
+        public BasicAirStrip(double width, double length): this(StandardUrl, width, length)
+        {
+            
+        }
+
+        /// <inheritdoc />
+        public override double CalculateArea()
+        {
+            return Width * Length;
+        }
+        
+        public override string ToString() => "BasicAirStrip[Width: " + Width + ", Length: " + Length + ", Score: " + 
+                                             Score + ", Status: " + Status + "]";
+    }
+}

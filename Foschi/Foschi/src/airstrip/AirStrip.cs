817aa1a9142ef4d209d97511991bf3ecc96d1092
Andrea Foschi <andrea.foschi13@studio.unibo.it>
2021-08-30 16:35:15 +0200

    Create AirStrip class and interface

---
+++ Foschi/Foschi/src/airstrip/AirStrip.cs
@@ -0,0 +1,45 @@
+﻿namespace Foschi.airstrip
+{
+    public abstract class AirStrip: IAirStrip
+    {
+        /// <summary>
+        /// Standard score for a plane
+        /// </summary>
+        private const int AirplaneScore = 100;
+
+        protected const string StandardUrl = "url_not_initialized";
+        /// <summary>
+        /// Status of the strip
+        /// </summary>
+        public AirStripStatus Status { get; set; }
+        /// <summary>
+        /// Actual score
+        /// </summary>
+        public int Score { get; set; }
+        /// <summary>
+        /// Url of the strip
+        /// </summary>
+        public string Url { get; set; }
+        
+        public AirStrip()
+        {
+            this.Status = AirStripStatus.Free;
+        }
+
+        public AirStrip(string url) : this()
+        {
+            this.Url = url;
+        }
+        /// <inheritdoc />
+        public void SetScore(int score)
+        {
+            this.Score += AirplaneScore;
+        }
+
+        /// <summary>
+        /// Calculate the area of the strip.
+        /// </summary>
+        /// <returns>The area itself</returns>
+        public abstract double CalculateArea();
+    }
+}
---
+++ Foschi/Foschi/src/airstrip/IAirStrip.cs
@@ -0,0 +1,12 @@
+﻿namespace Foschi.airstrip
+{
+    public interface IAirStrip
+    {
+
+        /// <summary>
+        /// Set the score when a plane lands.
+        /// </summary>
+        /// <param name="score">The score made when a plane is landed</param>
+        void SetScore(int score);
+    }
+}

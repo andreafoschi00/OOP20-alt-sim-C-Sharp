46222b6d4c9a0e050a9398ca405cb3dd38511247
daniel.rodilosso45 <daniel.rodilosso@outlook.com>
2021-09-06 19:03:07 +0200

    Create PlaneImpl.cs

---
+++ Rodilosso/AltSim/AltSim/model/Plane/PlaneImpl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animation.Model.Plane
+{
+    public class PlaneImpl
+    {
+        public PlaneImpl()
+        {
+
+        }
+    }
+}

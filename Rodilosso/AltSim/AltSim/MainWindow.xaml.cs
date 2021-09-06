f1c27ec3149347851d12a17f7378f20927306718
daniel.rodilosso45 <daniel.rodilosso@outlook.com>
2021-09-06 19:04:55 +0200

    Files xaml autogenerated

---
+++ Rodilosso/AltSim/AltSim/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.IO;
+
+namespace AltSim
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly double SCREEN_WIDTH = 1280;
+        private static readonly double SCREEN_HEIGHT = 920;
+
+        private static readonly string workingDirectory = Environment.CurrentDirectory;
+        private static readonly string UserHomeDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public static double GetWidth()
+        {
+            return SCREEN_WIDTH;
+        }
+
+        public static double GetHeight()
+        {
+            return SCREEN_HEIGHT;
+        }
+
+        public static string GetUserHomeDirectory()
+        {
+            return UserHomeDirectory;
+        }
+    }
+}

0bb88f97ed37c9c01efe2ad7c43d94269b761824
daniel.rodilosso45 <daniel.rodilosso@outlook.com>
2021-09-06 19:02:42 +0200

    Create LandingAnimation.cs

---
+++ Rodilosso/AltSim/AltSim/model/Animation/LandingAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Controls;
+
+namespace Animation.Model.Animation
+{
+    class LandingAnimation : Animation
+    {
+        private System.Timers.Timer timerAnimation;
+
+        public LandingAnimation(Image spriteToApplyAnimation) : base(spriteToApplyAnimation)
+        {
+
+        }
+
+        /// <inheritdoc/> 
+        public void Start()
+        {
+            SettingDefaultAnimationOptions();
+            GetLandingAnimation().Start();
+        }
+    }
+}

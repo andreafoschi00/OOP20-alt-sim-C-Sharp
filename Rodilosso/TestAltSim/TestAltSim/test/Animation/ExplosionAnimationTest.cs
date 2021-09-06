af0a3d4af5d8225c6fb6fc2b5af08bf288d8fdff
daniel.rodilosso45 <daniel.rodilosso@outlook.com>
2021-09-06 19:03:39 +0200

    Create ExplosionAnimationTest.cs

---
+++ Rodilosso/TestAltSim/TestAltSim/test/Animation/ExplosionAnimationTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace TestAltSim.Animation
+{
+    [TestClass]
+    public class ExplosionAnimationTest
+    {
+        private static readonly string workingDirectory = Environment.CurrentDirectory;
+        private static readonly string UserHomeDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+        private static readonly char UserSeparator = Path.DirectorySeparatorChar;
+        private static readonly int N_SPRITES = 50;
+
+        [Timeout(500)]
+        [TestMethod]
+        public void LoadImageTest()
+        {
+            try
+            {
+                string[] Documents = System.IO.Directory.GetFiles(UserHomeDirectory + UserSeparator + "src" + UserSeparator + "animation");
+               
+                int j = 0;
+
+                for (int k = 1; k < N_SPRITES; k++, j++)
+                {
+                    String urlImage = UserHomeDirectory + UserSeparator + "src" + UserSeparator + "animation" + UserSeparator + "explosion_" + k + ".png";
+
+                    char[] charsToTrim = { '\u005c' };
+                    string[] split = Documents[j].Split('\u005c');
+
+                    Assert.AreEqual(urlImage, (UserHomeDirectory + UserSeparator + "src" + UserSeparator + "animation" + UserSeparator + "explosion_" + k + ".png"));
+                }
+            }
+
+            catch (Exception e)
+            {
+                Assert.Fail();
+                System.Diagnostics.Debug.WriteLine(e.Message);
+            }
+        }
+    }
+}
